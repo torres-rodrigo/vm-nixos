847448054911ca003a6b21c3d06c3c6898d1b452
torres-rodrigo <torres.costa.rodrigo@gmail.com>
2026-08-05 21:30:11 -0300

    build failure fix attempt

--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -1,13 +1,11 @@
-{ ... }:
+{ config, ... }:
 
+let
+  homeDir = config.home.homeDirectory;
+in
 {
   home = {
     sessionVariables = {
-      XDG_CACHE_HOME = "$HOME/.cache";
-      XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME = "$HOME/.local/share";
-      XDG_STATE_HOME = "$HOME/.local/state";
-
       CARGO_HOME = "$XDG_DATA_HOME/cargo";
       RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
 
@@ -43,9 +41,9 @@
 
   xdg = {
     enable = true;
-    cacheHome = "$HOME/.cache";
-    configHome = "$HOME/.config";
-    dataHome = "$HOME/.local/share";
-    stateHome = "$HOME/.local/state";
+    cacheHome = "${homeDir}/.cache";
+    configHome = "${homeDir}/.config";
+    dataHome = "${homeDir}/.local/share";
+    stateHome = "${homeDir}/.local/state";
   };
 }
